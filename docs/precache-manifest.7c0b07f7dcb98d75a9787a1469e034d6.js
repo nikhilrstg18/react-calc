eae02d89aa31fc966fd37fb4dd4663a74f955726
Nikhil Rustagi <nikhil_rustagi@dellteam.com>
2019-01-26 22:13:45 +0530

    minor fix

---
+++ docs/precache-manifest.7c0b07f7dcb98d75a9787a1469e034d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "364ef4b3b164907ef7ba",
+    "url": "/react-calc/static/js/runtime~main.364ef4b3.js"
+  },
+  {
+    "revision": "1b68c74522dc71282408",
+    "url": "/react-calc/static/js/main.1b68c745.chunk.js"
+  },
+  {
+    "revision": "a6ca65584a1a91f1d3ba",
+    "url": "/react-calc/static/js/1.a6ca6558.chunk.js"
+  },
+  {
+    "revision": "1b68c74522dc71282408",
+    "url": "/react-calc/static/css/main.4092a120.chunk.css"
+  },
+  {
+    "revision": "8a2bc9143aa0a13bd2342fbf06cdb542",
+    "url": "/react-calc/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.0b42c2248771366a18cd109cf022347b.js"
+  "/react-calc/precache-manifest.7c0b07f7dcb98d75a9787a1469e034d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-calculator/index.html", {
+workbox.routing.registerNavigationRoute("/react-calc/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
